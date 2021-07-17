6b577f897a6907148da9e192f2f1d0075fbab590
mishail <WhiteBear05@yandex.ru>
2021-07-17 16:23:52 +0400

    -

--- restapi/restapi/settings.py
+++ restapi/restapi/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'restapi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'workers',
+        'NAME': 'medicaments',
         'USER' : 'mishail',
         'PASSWORD' : '1234',
         'HOST' : '127.0.0.1',
