0c8d8e15eb4128d42e9508f4cb7c13bee9cac989
Antonida <43772120+Rainbow42@users.noreply.github.com>
2021-08-02 19:45:29 +0400

    Update views.py

--- restapi/pharmacy/views.py
+++ restapi/pharmacy/views.py
@@ -3,47 +3,56 @@ from .serialisers import MedicationSerialiser, PharmacySerialiser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Pharmacy, Medication
+from rest_framework import status
 
 class MedicationView(APIView):
-
+    serializer_class = MedicationSerialiser
+    
     def get(self, request):
         if request.GET.get('id') != None:
             medications = Medication.objects.filter(id=request.GET.get('id')).first()
-            serialiser = MedicationSerialiser(medications)
+            serializer = self.serializer_class(medications)
+            
         elif request.GET.get('ph') != None:
             pharmacy = Pharmacy.objects.filter(id=request.GET.get('ph')).first()
             medications = pharmacy.medication_set.all()
-            serialiser = MedicationSerialiser(medications, many=True)
+            serializer = self.serializer_class(medications, many=True)
+            
         else:
             medications = Medication.objects.all()
-            serialiser = MedicationSerialiser(medications, many=True)
+            serializer = self.serializer_class(medications, many=True)
 
-        return Response({'medications': serialiser.data})
+        return Response({'medications': serializer.data})
 
     def post(self, request):
         medication = request.data.get('medication')
-        serialiser = MedicationSerialiser(data=medication)
-        if serialiser.is_valid(raise_exception=True):
-            serialiser.save()
-        return Response({'ok': 'Medication created successfully'})
+        serializer = self.serializer_class(data=medication)
+        
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def put(self, request):
         medication_saved = get_object_or_404(Medication.objects.all(), pk=request.GET.get('id'))
         data = request.data.get('medication')
-        serialiser = MedicationSerialiser(instance=medication_saved, data=data, partial=True)
-        if serialiser.is_valid(raise_exception=True):
-            serialiser.save()
-        return Response({'ok': 'Medication updated successfully'})
+        serializer = MedicationSerialiser(instance=medication_saved, data=data, partial=True)
+        
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class PharmacyView(APIView):
-
+    serializer_class = PharmacySerialiser
+    
     def get(self, request):
         if request.GET.get('city') != None:
             pharmacies = Pharmacy.objects.filter(p_city=request.GET.get('city'))
         else:
             pharmacies = Pharmacy.objects.all()
-        serialiser = PharmacySerialiser(pharmacies, many=True)
-
-        return Response({"pharmacies": serialiser.data})
+            
+        serializer = self.serializer_class(pharmacies, many=True)
+        return Response({"pharmacies": serializer.data})
 
