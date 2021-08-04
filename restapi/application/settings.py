8a5c85b9a1602b27635e0a0ff59dc3e2f1a148e5
UltraCube73 <WhiteBear05@yandex.ru>
2021-08-04 10:23:02 +0000

    -

--- restapi/application/settings.py
+++ restapi/application/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '1234'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '192.168.1.41']
 
 
 # Application definition
